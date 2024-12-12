047c3f30b9c644770be328d4108d8bddd0b43fdc
snuras <6snuras9@gmail.com>
2024-12-12 18:19:14 +0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import LoginPage from "./pages/LoginPage";
+
+ReactDOM.render(<LoginPage />, document.getElementById("root"));
