29b41311d2c3be11733d0cdd2aa6bb0515e74802
snuras <6snuras9@gmail.com>
2024-12-12 18:55:35 +0600

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import LoginPage from "./pages/LoginPage";
-
-ReactDOM.render(<LoginPage />, document.getElementById("root"));
